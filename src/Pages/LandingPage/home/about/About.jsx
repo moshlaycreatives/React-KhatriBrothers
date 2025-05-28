0f13f66094f75d0d03b7fa8276366d5ef2e06e04
mutahar123-dev <mutaharmuzammal16@gmail.com>
2025-05-29 02:31:17 +0500

    changing

--- src/Pages/LandingPage/home/about/About.jsx
+++ src/Pages/LandingPage/home/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./About.css"
-import { Button, Typography, useTheme } from '@mui/material'
+import { Box, Button, Typography, useTheme } from '@mui/material'
 import { useNavigate } from 'react-router'
 
 function About() {
@@ -21,13 +21,24 @@ navigate('about-us')
             </div>
         </div>
         <div className='home-about-text'>
-            <h2>Who We Are</h2>
+            {/* <h2>Who We Are</h2> */}
             <p>Khatri Brothers Academy was founded by Anmol and Shivam Khatri, the Top 6 contestants of SA RE GA MA PA Li’l Champs 2011, Anmol and Shivam, as the academy's creative directors and principal vocal instructors, bring their firsthand experience and youthful energy to the table. They understand the aspirations of aspiring musicians and can guide them effectively. and is committed to promoting and preserving India’s rich musical heritage.
             <br/>
             <br/>
             They encourage the real Indian music culture in the country. They have professionals who offer training in various Indian vocal genres, including Hindustani classical, Bhajan, Ghazals, Bollywood music, etc. They present the country at different stages and win a lot of awards in the music industry. They built self-confidence, stage presence, self-discipline, music appreciation, and performance skills crucial to being a self-oriented musician.</p>
             {/* <button>learn more <i class="fa-solid fa-arrow-right-long"></i></button> */}
-            <Button variant='contained' sx={{fontSize:'0.5rem'}} onClick={handleAbout}>Learn More</Button>
+
+            <Box sx={{fontSize:'0.5rem', display:'flex', alignItems:'center', justifyContent:'center'}}>
+            <Button variant='contained' sx={{fontSize:'0.5rem', display:'flex', alignItems:'center', justifyContent:'center'}} onClick={handleAbout}>Learn More</Button>
+
+            </Box>
+
+             </div>
+        <div className='home-about-image-main-div'>
+            <img src="./assets/about/tabla.png" alt="" />
+            <div  className=' home-about-inner-div'>
+                <img src="/bhai.JPG" alt=""  />
+            </div>
         </div>
     </div>
     </section>
